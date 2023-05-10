8ac8749a7fb38005a5f9bbbda75500123f738394
Khoi-Nguyen Tran <kndtran@gmail.com>
2023-05-10 02:59:16 -0400

    removed trailing spaces

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -198,7 +198,7 @@ class TestAccountService(TestCase):
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
         for key, value in headers.items():
-            self.assertEqual(response.headers.get(key), value)        
+            self.assertEqual(response.headers.get(key), value)
 
     def test_cors_security(self):
         """It should return a CORS header"""
